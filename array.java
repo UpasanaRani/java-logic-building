a53d5d53352100b8433b2774bcf99c18b52bc4ce
UpasanaRani <upasana00078@gmail.com>
2026-01-10 21:42:05 +0530

    Add files via upload

---
+++ array.java
@@ -0,0 +1,14 @@
+public class array {
+    public static void main(String[] args) {
+        float marks[] = { 2.5f, 3.9f, 5.2f, 6.8f };
+        float sum = 0;
+        for (float x : marks) {
+            sum += x;
+
+        }
+
+        System.out.println(sum);
+
+    }
+
+}
